7d956b21078bdfcf7b6daf95b5c743090a17bba1
otolba <omar16tolba@gmail.com>
2025-09-22 16:46:29 -0700

    Update chassis_subsystem.hpp

--- northstar-edu-project/src/control/chassis/chassis_subsystem.hpp
+++ northstar-edu-project/src/control/chassis/chassis_subsystem.hpp
@@ -68,7 +68,7 @@ public:
     It should look like this Ex. "ClassName(Thing thing1, Thing thing2);"
     */
     //STEP 1 HERE
-
+    ChassisSubsystem(tap::Drivers* drivers, const ChassisConfig& config);
     /*
     STEP 2: DECLARE METHODS
 
@@ -80,6 +80,8 @@ public:
     for the heading
     */
     //STEP 2 HERE
+    setVelocityFeildDrive();
+    driveBasedOnHeading();
 
     void initialize() override;
 
@@ -121,7 +123,9 @@ private:
         -rampControllers array or tap::algorithms::Ramp objects to limit acceleration on the wheels
     */
     //STEP 3 private HERE
-
+    std::array<float, static_cast<uint8_t>(MotorId::NUM_MOTORS)> desiredOutput;
+    std::array<Pid, static_cast<uint8_t>(MotorID::NUM_MOTORS)> pidControllers;
+    std::array<Ramp, static_cast<uint8_t>(MotorID::NUM_MOTORS)> rampControllers;
 protected:
     /* protected varables are able to be used within the class but not accesable from other classes just like a private var. 
     but any classes that inherit this class can use protected varables
@@ -129,7 +133,7 @@ protected:
 
      */
     //STEP 3 protected HERE
-
+    std::array<Motor, static_cast<uint8_t>(MotorID::NUM_MOTORS)> motorArray;
 };  // class ChassisSubsystem
 }  // namespace src::chassis
 
